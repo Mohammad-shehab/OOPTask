6bb8e4440f4abe46c0b1617fbe28fbe404dfc2e3
Mohammad-shehab <q8mlg@outlook.com>
2024-12-24 14:35:26 +0300

    Add project files.

---
+++ TaskOOP/Models/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskOOP.Models
+{
+
+
+
+    // Student class
+    public class Student
+    {
+
+        public double gpa;
+        public string name;
+        public int major;
+
+
+        public  string student(string name, double gpa, string major)
+        {
+
+            return $"Hello {name} your GPA is: {gpa}, Major: {major}. ";
+        }
+
+    }
+    //bonus
+    // using inheritance 
+    public class GraduateStudent : Student
+    {
+        public string year;
+        public string student(string name, double gpa, string major, string year)
+        {
+            return $"Hello {name} your GPA is: {gpa}, Major: {major}, Year: {year}. ";
+        }
+    }
+
+
+}
+
---
+++ TaskOOP/Program.cs
@@ -0,0 +1,31 @@
+﻿//task OOP
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskOOP.Models;
+
+
+
+
+Console.WriteLine("Name: ");
+string name = Console.ReadLine();
+Console.WriteLine("GPA: ");
+double gpa = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Major: ");
+string major = Console.ReadLine();
+
+////////////////////////////////////////////////////////////////////
+
+Student stu = new Student();
+Console.WriteLine(stu.student(name, gpa, major));
+
+////////////////////////////////////////////////////////////////////
+
+
+Console.WriteLine("Year: ");
+string year = Console.ReadLine();
+
+GraduateStudent und = new GraduateStudent();
+Console.WriteLine(und.student(name, gpa, major, year));
